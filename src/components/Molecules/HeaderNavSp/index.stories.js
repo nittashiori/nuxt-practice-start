158f4ff8acf05b13ac94b0ff233df8034a1f1dd5
nittashiori <nitta@impv.co.jp>
2022-11-16 08:33:26 +0900

    HeaderNavSpコンポーネント追加

---
+++ src/components/Molecules/HeaderNavSp/index.stories.js
@@ -0,0 +1,11 @@
+import HeaderNavSp from '.'
+
+export default {
+  title: 'Molecules/HeaderNavSp',
+  component: HeaderNavSp
+}
+
+export const $default = () => ({
+  components: { HeaderNavSp },
+  template: `<HeaderNavSp />`
+})
---
+++ src/components/Molecules/HeaderNavSp/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="header-nav-sp">
+    <ul class="header-nav-sp__list">
+      <li
+        class="header-nav-sp__item"
+        v-for="nav in navs"
+        :key="nav.index"
+      >
+        <a :href="nav.href">{{ nav.label }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import { navs } from "~/utils/header.js"
+
+export default {
+  data() {
+    return {
+      navs
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.header-nav-sp {
+  width: 200px;
+  height: 100vh;
+  padding: 84px 40px 0;
+  background: var(--color-primary);
+
+  &__list {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  &__item {
+    a {
+      display: block;
+      padding: 16px 0;
+      color: var(--color-white);
+      text-decoration: none;
+    }
+  }
+}
+</style>
