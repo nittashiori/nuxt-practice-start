26fcc3bad1725eea8f8a7def9907470d8bf6ee96
nittashiori <nitta@impv.co.jp>
2022-11-15 16:12:10 +0900

    HeaderNavコンポーネント追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,8 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: [
     '~/components',
-    '~/components/Atoms'
+    '~/components/Atoms',
+    '~/components/Molecules'
   ],
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
---
+++ src/components/Molecules/HeaderNav/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <nav class="header-nav">
+    <ul class="header-nav__list">
+      <li
+        class="header-nav__item"
+        v-for="list in lists"
+        :key="list.index"
+      >
+        <a :href=list.href>{{ list.label }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      lists: [
+        {
+          label: "ホーム",
+          href: "/",
+        },
+        {
+          label: "メッセージ",
+          href: "/message",
+        },
+        {
+          label: "会社概要",
+          href: "/company"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.header-nav {
+  font-size: 16px;
+
+  &__list {
+    display: flex;
+    padding: 0;
+    margin: 0;
+    list-style-type: none;
+
+    & > * + * {
+      margin-left: 30px;
+    }
+  }
+
+  a {
+    text-decoration: none;
+    color: var(--color-text);
+    transition: color 0.3s ease;
+
+    &:hover {
+      color: var(--color-primary);
+    }
+  }
+}
+</style>
